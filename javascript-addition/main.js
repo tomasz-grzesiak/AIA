954bf68110e180c9bd448ee6c39e90fe08f515fb
tgrzesiak <tom.grzes3141@gmail.com>
2020-04-06 00:46:09 +0200

    Fetch multiple subpages, asynchronous

--- javascript-addition/main.js
+++ javascript-addition/main.js
@@ -1,23 +1,36 @@
-const request = require('request');
-const jsdom = require('jsdom');
+const request = require("request");
+const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-request.get('https://www.mobileshop.eu/android-os/', (error, response, body) => {
+const url = "https://www.mobileshop.eu/android-os/";
+
+let resultList = []
+let pageNumber = 1;
+while (pageNumber < 10) {
+  request(`${url}${pageNumber > 1 ? "strona-"+pageNumber : ""}`, (error, response, body) => {
     if (error) {
-        console.log('Error occured: ', error);
-        return;
+      console.log("Error occured: ", error);
+      process.exit(1);
     }
-    let resultList = [];
-    const document = (new JSDOM(body)).window.document;
-    // const elements = document.querySelectorAll('div.product-name');
-    const names = document.querySelectorAll('div.product-name a');
-    const prizes = document.querySelectorAll('div.price div');
+
+    console.log(response.statusCode);
+
+    const document = new JSDOM(body).window.document;
+    const names = document.querySelectorAll("div.product-name a");
+    const prizes = document.querySelectorAll("div.price div");
     if (names.length !== prizes.length) {
-        console.log("somethin went wrong, fetched wrong amount of data...");
-        process.exit(1);
+      console.log("Something went wrong, fetched wrong amount of data...");
+      process.exit(1);
     }
     for (let i = 0; i < names.length; i++) {
-        resultList.push({name: names[i].innerHTML, RAM: names[i].innerHTML.split(' RAM')[0].split(' ').pop().split('G')[0], prize: prizes[i].innerHTML.split('&')[0]});
+      resultList.push({
+        name: names[i].innerHTML,
+        RAM: names[i].innerHTML.split(" RAM")[0].split(" ").pop().split("G")[0],
+        prize: prizes[i].innerHTML.split("&")[0],
+      });
     }
     console.log(resultList);
-})
+    console.log(resultList.length);
+  });
+  pageNumber++;
+}
